343484bf55c20548d79f1c16e27fcb5ffe39e8c3
AleksandrBelousov <tortyt1@gmail.com>
2017-06-28 15:10:44 +0400

    Added System and Balance Controllers

---
+++ EthereumSamurai/EthereumSamurai/Responses/BalanceResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace EthereumSamurai.Responses
+{
+    [DataContract]
+    public class BalanceResponse
+    {
+        [DataMember(Name = "amount")]
+        public string Amount { get; set; }
+    }
+}
