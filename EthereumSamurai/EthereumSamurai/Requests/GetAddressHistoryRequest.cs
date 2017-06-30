d99029e3e8b886571aed970cb135a966e4b6b7f6
AleksandrBelousov <tortyt1@gmail.com>
2017-06-30 12:09:20 +0400

    Imporoved transaction querying filter building

--- EthereumSamurai/EthereumSamurai/Requests/GetAddressHistoryRequest.cs
+++ EthereumSamurai/EthereumSamurai/Requests/GetAddressHistoryRequest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace EthereumSamurai.Requests
     public class GetAddressHistoryRequest : PagingBaseRequest
     {
         [FromRoute]
+        [Required]
         [DataMember(Name ="address")]
         public string Address { get; set; }
     }
