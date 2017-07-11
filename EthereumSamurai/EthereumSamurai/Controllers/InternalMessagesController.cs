4d3234d1fe2af5bc508123a910b07528781ca98d
AleksandrBelousov <tortyt1@gmail.com>
2017-07-11 12:27:42 +0400

    Added querying for internal messages

---
+++ EthereumSamurai/EthereumSamurai/Controllers/InternalMessagesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EthereumSamurai.Core.Services;
+using EthereumSamurai.Models.Query;
+using EthereumSamurai.Models.Blockchain;
+using AutoMapper;
+using EthereumSamurai.Responses;
+using EthereumSamurai.Requests;
+using EthereumSamurai.Filters;
+
+namespace EthereumSamurai.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class InternalMessagesController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IInternalMessageService _internalMessageService;
+
+        public InternalMessagesController(IInternalMessageService internalMessageService, IMapper mapper)
+        {
+            _internalMessageService = internalMessageService;
+            _mapper = mapper;
+        }
+
+        [Route("{address}")]
+        [HttpGet]
+        [ProducesResponseType(typeof(FilteredInternalMessageResponse), 200)]
+        [ProducesResponseType(typeof(ApiException), 400)]
+        [ProducesResponseType(typeof(ApiException), 500)]
+        public async Task<IActionResult> GetForAddress(GetAddressInternalMessageHistoryRequest request)
+        {
+            string address = request.Address.ToLower();
+            var transactionQuery = new InternalMessageQuery()
+            {
+                FromAddress = address,
+                ToAddress = address,
+                StartBlock = request.StartBlock,
+                StopBlock = request.StopBlock,
+                Start = request.Start,
+                Count = request.Count
+            };
+
+            List<InternalMessageModel> messages = (await _internalMessageService.GetAsync(transactionQuery)).ToList();
+           
+            return ProcessResponse(messages);
+        }
+
+        private IActionResult ProcessResponse(List<InternalMessageModel> messages)
+        {
+            List<InternalMessageResponse> response = new List<InternalMessageResponse>(messages.Count);
+            messages.ForEach(transaction =>
+            {
+                InternalMessageResponse trResponse = _mapper.Map<InternalMessageResponse>(transaction);
+                response.Add(trResponse);
+            });
+
+            return new JsonResult(new FilteredInternalMessageResponse()
+            {
+                Messages = response
+            });
+        }
+    }
+}
---
+++ EthereumSamurai/EthereumSamurai/Requests/GetAddressInternalMessageHistoryRequest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace EthereumSamurai.Requests
+{
+    [DataContract]
+    public class GetAddressInternalMessageHistoryRequest : PagingBaseRequest
+    {
+        [FromRoute]
+        [Required]
+        [DataMember(Name ="address")]
+        public string Address { get; set; }
+
+        [FromQuery]
+        [DataMember(Name = "startBlock")]
+        public ulong? StartBlock { get; set; }
+
+        [FromQuery]
+        [DataMember(Name = "stopBlock")]
+        public ulong? StopBlock { get; set; }
+    }
+}
