93237b3c7267eb4238453f671591fdff2d62c22e
Yury Glushkov <mail@glushkov.us>
2017-08-16 11:43:40 +0300

    Response type attribute fixed for GetForAddress method of Erc20BalanceController

--- EthereumSamurai/EthereumSamurai/Controllers/Erc20BalanceController.cs
+++ EthereumSamurai/EthereumSamurai/Controllers/Erc20BalanceController.cs
@@ -22,7 +22,7 @@ namespace EthereumSamurai.Controllers
 
         [Route("getErc20Balance/{address}")]
         [HttpPost]
-        [ProducesResponseType(typeof(BalanceResponse), 200)]
+        [ProducesResponseType(typeof(Erc20BalanceResponse), 200)]
         [ProducesResponseType(typeof(ApiException), 400)]
         [ProducesResponseType(typeof(ApiException), 500)]
         public async Task<IActionResult> GetForAddress([FromRoute] string address, [FromBody] IEnumerable<string> contracts)
